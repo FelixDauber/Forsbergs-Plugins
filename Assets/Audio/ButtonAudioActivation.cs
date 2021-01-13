e093591bc88956992df1864dbff01f07498aa910
Amy S <enechan444@gmail.com>
2021-01-13 13:01:12 +0100

    Audio 02
    Added a test scene.

---
+++ Assets/Audio/ButtonAudioActivation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAudioActivation : MonoBehaviour
+{
+    //Activation for Hover, Press Possitive and Negative.
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
