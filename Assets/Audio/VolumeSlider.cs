d86cf51ae1b102b077a9300db2a1da19a2de7e16
qreenify <adam@tapper.one>
2021-01-14 13:14:06 +0100

    adds VolumeSlider prefab and edits the VolumeSlider-script

--- Assets/Audio/VolumeSlider.cs
+++ Assets/Audio/VolumeSlider.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
 
 public class VolumeSlider : MonoBehaviour
 {
 
     public Slider volumeSlider;
-    private AudioSource audioSource;
+    public AudioManager audioManager;
 
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        volumeSlider.value = audioSource.volume;
+        audioManager = gameObject.GetComponent<AudioManager>();
+        volumeSlider.value = audioManager.sounds[0].volume;
     }
 
     // Update is called once per frame
     void Update()
     {
+        audioManager.sounds[0].volume = volumeSlider.value;
     }
 }
