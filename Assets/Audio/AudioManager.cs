23d671353cb8f19ac46bb4556c4780208f0b9e8b
Amy S <enechan444@gmail.com>
2021-01-14 17:46:11 +0100

    Audio 04
    DERP

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -1,25 +1,42 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Events;
 using System;
+using Plugins.ListMenuPlugin.Scripts;
 
 public class AudioManager : MonoBehaviour
 {
+    //UnityEvent onButtonClick = new UnityEvent(); 
+    UnityEvent<string> onButtonClick;
     public Sound[] sounds;
     void Awake()
     {
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-
-            // Use this as the call command for the Button Sounds
-            // FindObjectOfType<AudioManager>().Play("Button1");
         }
     }
-
+    
+    public void Start()
+    {
+        //onButtonClick.AddListener(ButtonSound);
+        var menuHolder = GetComponent<MenuHolder>();
+        menuHolder.menu.onButtonClick.AddListener(Play);
+    }
+    
     public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+    public void Update()
+    {
+        if (onButtonClick != null)
+        {
+            ButtonSound();
+        }
+    }
+    void ButtonSound()
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Play();
--- Assets/Audio/Sound.cs
+++ Assets/Audio/Sound.cs
@@ -1,15 +1,38 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Events;
 [System.Serializable]
 public class Sound
 {
+    UnityEvent ButtonSound = new UnityEvent();
+
     public string name;
     public AudioClip clip;
     [Range(0f, 1f)]
     public float volume;
-    [Range(0.1f, 3f)]
+    [Range(1.0f, 3f)]
     public float pitch;
 
     [HideInInspector]
     public AudioSource source;
+
+    public void AudioSetup()
+    {
+        source.clip = clip;
+        source.volume = volume;
+        source.pitch = pitch;
+        source.name = name;
+
+        // public float volume {get => volume; set => ;}
+        // ButtonSound
+        // Menu.event.AddListener(Play)
+
+        // Use this as the call command for the Button Sounds
+        // FindObjectOfType<AudioManager>().Play("Button1");
 }
+    public void Start()
+    {
+        ButtonSound.AddListener(AudioSetup);
+    }
+}
+
