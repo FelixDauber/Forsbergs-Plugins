f80388befed6513ace20327592aeb762ff28bc00
qreenify <adam@tapper.one>
2021-01-14 17:59:15 +0100

    adds mutesounds

--- Assets/Audio/VolumeSlider.cs
+++ Assets/Audio/VolumeSlider.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using Slider = UnityEngine.UI.Slider;
 
-public class VolumeSlider : MonoBehaviour
+public class VolumeSlider: MonoBehaviour
 {
-
     public Slider volumeSlider;
     public AudioManager audioManager;
-
-    
     // Start is called before the first frame update
     void Start()
     {
         audioManager = gameObject.GetComponent<AudioManager>();
         volumeSlider.value = audioManager.sounds[0].volume;
     }
-
     // Update is called once per frame
     void Update()
     {
