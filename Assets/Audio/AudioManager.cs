1d433e7f447790af9a0b99eb5b808809c917c4db
Amy S <enechan444@gmail.com>
2021-01-14 17:54:27 +0100

    Audio 05
    Cleaned up Derp.
    Sound should now play when buttons are pressed.
    If you don't hear anything make sure that you have the volume on and maybe try headset.

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -6,7 +6,6 @@ using Plugins.ListMenuPlugin.Scripts;
 
 public class AudioManager : MonoBehaviour
 {
-    //UnityEvent onButtonClick = new UnityEvent(); 
     UnityEvent<string> onButtonClick;
     public Sound[] sounds;
     void Awake()
@@ -14,29 +13,17 @@ public class AudioManager : MonoBehaviour
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
+            s.AudioSetup();
         }
     }
     
     public void Start()
     {
-        //onButtonClick.AddListener(ButtonSound);
         var menuHolder = GetComponent<MenuHolder>();
         menuHolder.menu.onButtonClick.AddListener(Play);
     }
     
     public void Play (string name)
-    {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        s.source.Play();
-    }
-    public void Update()
-    {
-        if (onButtonClick != null)
-        {
-            ButtonSound();
-        }
-    }
-    void ButtonSound()
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Play();
